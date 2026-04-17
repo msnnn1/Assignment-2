e8cc454d3d4d3de11a22f62b7c7137a846b6b494
Manooshi Niraula <niraulamanooshii@gmail.com>
2026-04-17 19:54:27 +0545

    done

--- dice.py
+++ dice.py
@@ -3,6 +3,9 @@ import random
 dice = random.randint(1, 6)
 guess = int(input("Guess the dice (1-6): "))
 
+if guess < 1 or guess > 6:
+    print("Please guess between 1 and 6 next time!")
+
 print("Dice value:", dice)
 
 if guess == dice:
